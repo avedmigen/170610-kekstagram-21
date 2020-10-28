f942a46f9d16c405360dc35e92f64c1c7de437cb
Dmitry Averin <avedmigen@gmail.com>
2020-10-28 19:55:54 +0300

    Исправляет замечания наставника

--- js/pictures.js
+++ js/pictures.js
@@ -2,32 +2,26 @@
 
 (() => {
   window.load((photos) => {
-    let picturesContainer = document.querySelector(`.pictures`);
+    const PICTURES_CONTAINER = document.querySelector(`.pictures`);
+    const pictureTemplate = document.querySelector(`#picture`)
+      .content
+      .querySelector(`.picture`);
+
+    let renderPhotos = (photo) => {
+      let photoElement = pictureTemplate.cloneNode(true);
+      photoElement.querySelector(`.picture__img`).src = photo.url;
+      photoElement.querySelector(`.picture__comments`).textContent = photo.comments.length;
+      photoElement.querySelector(`.picture__likes`).textContent = photo.likes;
+      return photoElement;
+    };
+
     let fragment = document.createDocumentFragment();
 
     for (let i = 0; i < photos.length; i++) {
-      let newElement = document.createElement(`a`);
-      newElement.className = `picture`;
-      newElement.onmouseover = () => {
-        newElement.style.cursor = 'pointer';
-      };
-      newElement.onmouseleave = () => {
-        newElement.style.cursor = '';
-      };
-      newElement.innerHTML = `
-    <img class="picture__img" src="${photos[i].url}" width="182" height="182" alt="Случайная фотография">
-    <p class="picture__info">
-      <span class="picture__comments">${photos[i].comments.length}</span>
-      <span class="picture__likes">${photos[i].likes}</span>
-    </p>`;
-
-      fragment.appendChild(newElement);
-      picturesContainer.appendChild(fragment);
-
-      // 4.4. При нажатии на любую из миниатюр, показывается блок .big-picture
-      newElement.addEventListener('click', () => {
-        /*        console.log('показывается блок .big-picture');*/
-      });
+      fragment.appendChild(renderPhotos(photos[i]));
     }
-  }, () => {});
+
+    PICTURES_CONTAINER.appendChild(fragment);
+  });
+
 })();
