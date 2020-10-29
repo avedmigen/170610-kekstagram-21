0690f85dcb781c8e677e8e0f9e13530b26f3d2c5
Dmitry Averin <avedmigen@gmail.com>
2020-10-29 16:02:46 +0300

    Добавляет фильтры

--- js/pictures.js
+++ js/pictures.js
@@ -2,13 +2,18 @@
 
 (() => {
   window.load((photos) => {
-    const PICTURES_CONTAINER = document.querySelector(`.pictures`);
-    const pictureTemplate = document.querySelector(`#picture`)
+    // Если данные пришли успешно
+    // То нарисуй превью фоток на странице
+    const cleanDataPhotos = photos.slice();
+
+    const photoContainer = document.querySelector(`.pictures`);
+
+    const srcTmpl = document.querySelector(`#picture`)
       .content
       .querySelector(`.picture`);
 
-    let renderPhotos = (photo) => {
-      let photoElement = pictureTemplate.cloneNode(true);
+    let setPhotoTemplate = (photo, template) => {
+      let photoElement = template.cloneNode(true);
       photoElement.querySelector(`.picture__img`).src = photo.url;
       photoElement.querySelector(`.picture__img`).alt = photo.description;
       photoElement.querySelector(`.picture__comments`).textContent = photo.comments.length;
@@ -16,13 +21,63 @@
       return photoElement;
     };
 
-    let fragment = document.createDocumentFragment();
+    let drawPhotos = (photoArr, container, template, srctmpl) => {
+      let fragment = document.createDocumentFragment();
+      for (let i = 0; i < photoArr.length; i++) {
+        fragment.appendChild(template(photoArr[i], srctmpl));
+      }
+      container.appendChild(fragment);
+    };
 
-    for (let i = 0; i < photos.length; i++) {
-      fragment.appendChild(renderPhotos(photos[i]));
+    drawPhotos(cleanDataPhotos, photoContainer, setPhotoTemplate, srcTmpl);
+
+    // И покажи фильтры
+    const filtersContainer = document.querySelector(`.img-filters`);
+    filtersContainer.classList.toggle(`img-filters--inactive`);
+
+    // И cлушай клики на фильтрах
+    const filters = document.querySelectorAll(`.img-filters__button`);
+    let filterName = 0;
+
+    for (let filter of filters) {
+      filter.addEventListener(`click`, (e) => {
+        e.preventDefault();
+        filterName = filter.id;
+
+        switch (filterName) {
+          case `filter-default`:
+            window.utils.getRenderedPhotos();
+            window.utils.removeRenderedPhotos();
+            drawPhotos(cleanDataPhotos, photoContainer, setPhotoTemplate, srcTmpl);
+            break;
+
+          case `filter-random`:
+            // Перемешай массив с фотками
+            const shuffled =
+              cleanDataPhotos.slice()
+              .map((a) => ({sort: Math.random(), value: a}))
+              .sort((a, b) => a.sort - b.sort)
+              .map((a) => a.value);
+
+            shuffled.length = 10;
+            window.utils.getRenderedPhotos();
+            window.utils.removeRenderedPhotos();
+            drawPhotos(shuffled, photoContainer, setPhotoTemplate, srcTmpl);
+            break;
+
+          case `filter-discussed`:
+            const sorted = cleanDataPhotos.slice();
+            // Отсортируй массив по значению поля
+            sorted.sort((a, b) => b.comments.length - a.comments.length);
+            window.utils.getRenderedPhotos();
+            window.utils.removeRenderedPhotos();
+            drawPhotos(sorted, photoContainer, setPhotoTemplate, srcTmpl);
+            break;
+
+          default:
+            break;
+        }
+      });
     }
-
-    PICTURES_CONTAINER.appendChild(fragment);
   });
-
 })();
---
+++ js/utils.js
@@ -0,0 +1,36 @@
+'use strict';
+
+(() => {
+
+  // Сгенерируй рандомное число из диапазона
+  const getMinMaxRandomNumber = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  // Найди все фотки на странице
+
+  const getRenderedPhotos = () => {
+    const renderedPhotos = document.querySelectorAll(`.picture`);
+    return renderedPhotos;
+  };
+
+  // Удали все фотки на странице
+
+  const removeRenderedPhotos = () => {
+    const renderedPhotos = document.querySelectorAll(`.picture`);
+    for (let photo of renderedPhotos) {
+      photo.remove();
+    }
+  };
+
+  // Экспортируй утилиты
+
+  window.utils = {
+    getMinMaxRandomNumber,
+    getRenderedPhotos,
+    removeRenderedPhotos,
+  };
+
+})();
