d7d8c50f1ba2e5ab822d77ec6ae05ec040e70b05
Dmitry Averin <avedmigen@gmail.com>
2020-11-03 17:28:57 +0300

    Начинает использовать делегирование и выносит рендер бигпикчи в отдельный модуль

---
+++ js/bigpicture.js
@@ -0,0 +1,34 @@
+'use strict';
+
+(() => {
+  const renderBigPicture = (photo) => {
+
+    // Найди контейнер бигпикчи
+    const bigPic = document.querySelector(`.big-picture__preview`);
+    // Замени путь к файлу и альт бигпикчи
+    const bigPicImg = bigPic.querySelector(`.big-picture__img > img`);
+    bigPicImg.src = photo.url;
+    bigPicImg.alt = photo.description;
+    // Замени количество лайков
+    const bigPicLikes = bigPic.querySelector(`.likes-count`);
+    bigPicLikes.textContent = photo.likes;
+    // Замени количество комментов
+    const bigPicСommentsCount = bigPic.querySelector(`.comments-count`);
+    bigPicСommentsCount.textContent = photo.comments.length;
+    // Замени контент комментов
+    const bigPicСommentsList = bigPic.querySelector(`.social__comments`);
+    const bigPicСomment = bigPicСommentsList.querySelector(`.social__comment`);
+    // Замени контент описания бигпикчи
+    const bigPicDesc = bigPic.querySelector(`.social__caption`);
+    bigPicDesc.textContent = photo.description;
+    // Покажи бигпикчу
+    document.querySelector(`.big-picture`).classList.remove(`hidden`);
+
+    console.log(`Бигпича отрендерена`);
+  };
+
+  window.bigpicture = {
+    renderBigPicture,
+  };
+
+})();
--- js/pictures.js
+++ js/pictures.js
@@ -21,6 +21,14 @@
       photoElement.querySelector(`.picture__img`).alt = photo.description;
       photoElement.querySelector(`.picture__comments`).textContent = photo.comments.length;
       photoElement.querySelector(`.picture__likes`).textContent = photo.likes;
+
+      const onPhotoElementClick = (e) => {
+        e.preventDefault();
+
+        window.bigpicture.renderBigPicture(photo);
+      };
+
+      photoElement.addEventListener(`click`, onPhotoElementClick);
       return photoElement;
     };
 
@@ -86,119 +94,7 @@
             break;
         }
         drawFilteredPhotos(sorted);
-        listenClicksOnThumbnails();
       });
     }
-
-    // Слушай клики на превьюшках
-    const listenClicksOnThumbnails = () => {
-      const previews = document.querySelectorAll(`.picture`);
-      const bigPicContainer = document.querySelector(`.big-picture`);
-
-      for (let preview of previews) {
-        preview.addEventListener(`click`, (e) => {
-          e.preventDefault();
-          // Дай урл превьюшки
-          const previewImg = preview.querySelector(`.picture__img`);
-          let previewUrl = previewImg.attributes.src.value;
-          // Найди в массиве с фотками объект, соответстующий урлу превьюшки
-          const pictureUrl = (picture) => (picture.url === previewUrl ? true : false);
-          const pictureObj = cleanDataPhotos.filter((pictureUrl));
-          // Найди контейнер бигпикчи
-          const bigPic = bigPicContainer.querySelector(`.big-picture__preview`);
-          // Замени путь к файлу и альт бигпикчи
-          const bigPicImg = bigPic.querySelector(`.big-picture__img > img`);
-          bigPicImg.src = pictureObj[0].url;
-          bigPicImg.alt = pictureObj[0].description;
-          // Замени количество лайков
-          const bigPicLikes = bigPic.querySelector(`.likes-count`);
-          bigPicLikes.textContent = pictureObj[0].likes;
-          // Замени количество комментов
-          const bigPicСommentsCount = bigPic.querySelector(`.comments-count`);
-          bigPicСommentsCount.textContent = pictureObj[0].comments.length;
-          // Замени контент комментов
-          const bigPicСommentsList = bigPic.querySelector(`.social__comments`);
-          const bigPicСomment = bigPicСommentsList.querySelector(`.social__comment`);
-          // Замени контент описания бигпикчи
-          const bigPicDesc = bigPic.querySelector(`.social__caption`);
-          bigPicDesc.textContent = pictureObj[0].description;
-
-          // Шаблон коммента для заполнения данными
-          const bigPicСommentTmpl = (num) => {
-            let commentElement = bigPicСomment.cloneNode(true);
-            let commentAvatar = pictureObj[0].comments[num].avatar;
-            let commentAlt = pictureObj[0].comments[num].name;
-            let commentSocialText = pictureObj[0].comments[num].message;
-
-            commentElement.querySelector(`.social__picture`).src = commentAvatar;
-            commentElement.querySelector(`.social__picture`).alt = commentAlt;
-            commentElement.querySelector(`.social__text`).textContent = commentSocialText;
-            return commentElement;
-          };
-
-          // Удали существующие комменты из разметки
-          while (bigPicСommentsList.firstChild) {
-            bigPicСommentsList.removeChild(bigPicСommentsList.firstChild);
-          }
-
-          // Приготовься добавлять комменты из полученных данных
-          const bigPicСomments = () => {
-            let fragment = document.createDocumentFragment();
-            for (let i = 0; i < pictureObj[0].comments.length; i++) {
-              fragment.appendChild(bigPicСommentTmpl(i));
-            }
-            bigPicСommentsList.appendChild(fragment);
-          };
-
-          // Нарисуй комменты с данными в бигпикче
-          bigPicСomments();
-
-          // Спрячь блоки счётчика комментариев и загрузки новых комментариев
-          bigPic.querySelector(`.social__comment-count`).classList.add(`hidden`);
-          bigPic.querySelector(`.comments-loader`).classList.add(`hidden`);
-
-          // Добавь на <body> класс modal-open, чтобы контейнер с фотографиями позади не прокручивался при скролле.
-          document.body.classList.add(`modal-open`);
-
-          // Покажи бигпикчу и установи флаг, что она открыта
-          let getIsBigPicOpened = (flag = false) => {
-            let isBigPicOpened = flag;
-            return isBigPicOpened;
-          };
-
-          window.pictures = {
-            getIsBigPicOpened,
-          };
-
-          const shoeBigPic = () => {
-            bigPicContainer.classList.remove(`hidden`);
-            window.pictures.isBigPicOpened = true;
-          };
-
-          shoeBigPic();
-        });
-      }
-
-      // Закрой попап бигпикчи по клику на кнопке с крестом
-      const bigPictureCancel = document.querySelector(`.big-picture__cancel`);
-      bigPictureCancel.addEventListener(`click`, (e) => {
-        e.preventDefault();
-        bigPicContainer.classList.add(`hidden`);
-        document.body.classList.remove(`modal-open`);
-      });
-
-      // Закрой попап бигпикчи по нажатию Escape
-      window.addEventListener(`keydown`, (e) => {
-        if (e.code === `Escape`) {
-          e.preventDefault();
-          e.preventDefault();
-          bigPicContainer.classList.add(`hidden`);
-          document.body.classList.remove(`modal-open`);
-        }
-      });
-    };
-
-    listenClicksOnThumbnails();
-
   });
 })();
