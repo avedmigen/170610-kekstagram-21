7072f847eed9a628850c5a5ca7cad9fbb5be4181
Dmitry Averin <avedmigen@gmail.com>
2020-11-06 19:26:14 +0300

    Добавляет отображение загружаемого файла в превью и эффектах

---
+++ js/userimage.js
@@ -0,0 +1,39 @@
+'use strict';
+
+(() => {
+  const FILE_TYPES = [`gif`, `jpg`, `jpeg`, `png`];
+
+  const fileChooser = document.querySelector(`#upload-file`);
+  const preview = document.querySelector(`.img-upload__preview img`);
+  const effectPreviews = document.querySelectorAll(`.effects__preview`);
+
+  const onFileChooserChange = () => {
+    const file = fileChooser.files[0];
+    const fileName = file.name.toLowerCase();
+
+    const matches = FILE_TYPES.some((it) => {
+      return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      const reader = new FileReader();
+
+      const onReaderLoad = () => {
+        preview.src = reader.result;
+
+        effectPreviews.forEach((item) => {
+          item.style.backgroundImage = `url(${reader.result})`;
+        });
+
+      };
+
+      reader.addEventListener(`load`, onReaderLoad);
+
+      reader.readAsDataURL(file);
+    }
+
+  };
+
+  fileChooser.addEventListener(`change`, onFileChooserChange);
+
+})();
